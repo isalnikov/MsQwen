a8c71acfa8e9267f16413c68d1e71fe689014b74
isalnikov <igor.salnikov@stoloto.ru>
2026-04-01 12:48:42 +0300

    add file

---
+++ src/main/java/com/isalnikov/msqwen/MsQwen.java
@@ -0,0 +1,13 @@
+
+package com.isalnikov.msqwen;
+
+/**
+ *
+ * @author Igor Salnikov <isalnikov1@gmail.com>
+ */
+public class MsQwen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello MsQwen AI!");
+    }
+}
